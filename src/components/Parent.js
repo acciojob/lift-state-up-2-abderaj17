738a0e6c8a30432b3115a1af1bdd94ec9242afd9
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-21 17:33:13 +0530

    cl

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Parent = () => {
+    const [text, setText] = useState("");
+
+    const handleChange = (e)=>{
+        setText = e.target.value;
+        text(setText);
+    }
+    
   return (
     <div>
         <div className='parent'>
          <h1>Parent Component</h1>
-           <div className='showText'></div>
+           <div className='showText' onChange={handleChange}></div>
 
            <div className='child'>
            <h2>Child Component</h2>
-           <input type='text' id='write' />
+           <input type='text' id='write' onChange={handleChange} />
         </div>
         </div>
       
